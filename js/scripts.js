80ae37218781cbebe714af9e6edaafe4ea92d59d
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-01-13 10:03:19 -0800

    added rough working index.html file and rough working js/scripts.js file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,33 @@
-function handleCalculation(event) {
+function handleSurvey(event) {
   event.preventDefault();
-  const operator = document.querySelector("input[name='operator']:checked").value;
+
+  let q1 = document.getElementById("q1");
+  let q2 = document.getElementById("q2");
+  let q3 = document.getElementById("q3");
+  let q4 = document.getElementById("q4");
+  let q5 = document.getElementById("q5");
+
+  if (q1.checked == true) {
+    output = "python"
+  }
+  else if (q2.checked == true) {
+    output = "ruby"
+  }
+  else if (q3.checked == true && q4.checked == true) {
+    output = "c#"
+  }
+  else if (q5.checked == true) {
+    output = "javascript"
+  }
+  else {
+    output = ".net"
+  }
+
+
+  document.getElementById("output").innerText = output;
+}
+
+window.addEventListener("load", function() {
+  const form = document.getElementById("questions");
+  form.addEventListener("submit", handleSurvey);
+});
